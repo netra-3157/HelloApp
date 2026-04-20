74376b9285a1e19bdf440732a4390ad2644555cc
netra-3157 <netravenkat2531@gmail.com>
2026-04-20 18:39:59 +0530

    Use case6

--- Hello.java
+++ Hello.java
@@ -1,24 +1,25 @@
 public class Hello{
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 
-        String name = "World";
-
-        if (args.length > 0) {
-            StringBuilder nameBuilder = new StringBuilder();
-            boolean first = true;
-
-            for (String n : args) {
-                if (!first) {
-                    nameBuilder.append(", ");
-                }
-                nameBuilder.append(n);
-                first = false;
-            }
-
-            name = nameBuilder.toString();
+        // If no names are given
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
         }
 
-        System.out.println("Hello, " + name + "!");
+        // Use StringBuilder to combine names
+        StringBuilder nameBuilder = new StringBuilder();
+
+        // Enhanced for loop to iterate through arguments
+        for (String name : args) {
+            nameBuilder.append(name).append(", ");
+        }
+
+        // Remove the last comma and space
+        String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+
+        // Print greeting
+        System.out.println("Hello, " + names + "!");
         
     }
 }
