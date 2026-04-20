3329c1542e0fd3f218e04468baebd42ff01e9ca2
netra-3157 <netravenkat2531@gmail.com>
2026-04-20 13:10:52 +0530

    comment

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello-App {
+	public static void main(String[] args){
+		//variable and datatypes
+	int num=100;
+	System.out.println(num);
+	System.out.println("Hello All...");
+	}
+}
+}
