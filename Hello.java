ad30b5c91cee8bbbf93b17c1fdf8935974e07fb6
netra-3157 <netravenkat2531@gmail.com>
2026-04-20 18:43:08 +0530

    Use case7

--- Hello.java
+++ Hello.java
@@ -1,28 +1,20 @@
 public class Hello{
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 
-        // If no names are given
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-            return;
+        // Default name if no arguments are provided
+        String names = "World";
+
+        // If user provides names, join them with comma and space
+        if (args.length > 0) {
+            names = String.join(", ", args);
         }
-
-        // Use StringBuilder to combine names
-        StringBuilder nameBuilder = new StringBuilder();
-
-        // Enhanced for loop to iterate through arguments
-        for (String name : args) {
-            nameBuilder.append(name).append(", ");
-        }
-
-        // Remove the last comma and space
-        String names = nameBuilder.substring(0, nameBuilder.length() - 2);
 
         // Print greeting
         System.out.println("Hello, " + names + "!");
         
     }
 }
+
 
 
 
