125802d33b2eb43a5297a1a75176d186bc153f5c
netra-3157 <netravenkat2531@gmail.com>
2026-04-20 18:07:32 +0530

    New usecase

--- Hello.java
+++ Hello.java
@@ -1,11 +1,18 @@
 public class Hello {
-	public static void main(String[] args){
-		//variable and datatypes
-	int num=100;
-	System.out.println(num);
-	System.out.println("Hello All...");
+	public static void main(String[] args) {
 
-	
-	}
+        String name;
+
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
+
+        System.out.println("Hello, " + name + "!");
+
+		
+    }
+
 }
 
